113a41e05bce43fbea106283eb40403864a03c4f
Ellie311 <64198864+Elizabeth0311@users.noreply.github.com>
2021-03-19 16:41:41 +0900

    질문추가

--- leetcode1672.py
+++ leetcode1672.py
@@ -1,6 +1,16 @@
+# 1672. Richest Customer Wealth
 
-accounts = [[1,5],[7,3],[3,5]]
+# 1. 리스트 안 리스트의 요소를 다 더한다. 
+# 2. 각 리스트 합의 크기를 비교한다.
+# 3. 제일 큰 수 출력한다.
 
-therichest = ([sum(i) for i in accounts])
+accounts = [[1,5],[7,3],[3,5]]  
 
-print (max(therichest))
+therichest = ([sum(i) for i in accounts]) 
+
+ # 리스트 내 리스트 합 구하기 
+  # 질문? 1. accounts 내 리스트가 어떻게 i로 들어가는 것일까? 
+  #      2. []가 의미하는 것은? 
+
+  
+  print (max(therichest))
