670db11ab6db11be848964c43b847c35a678befe
Ellie311 <64198864+Elizabeth0311@users.noreply.github.com>
2021-03-21 18:39:43 +0900

    Update leetcode1470.py

--- leetcode1470.py
+++ leetcode1470.py
@@ -6,11 +6,26 @@
 # 4. 나머지 리스트와 3에서 구한 리스트 합치기 (못함) 
 
 nums = [2,5,1,3,4,7]
-# nums.insert(2,3)
-# print(nums)
 
 numslength = int(len(nums))  #리스트 길이
-cutting = int(numslength / 2)  #반으로 나눈 값
+cut = int(numslength / 2)  #반으로 나눈 값
 
-a = (nums[cutting : ]) # 나눈 값 부터 슬라이싱 
-b = (nums[ :cutting ]) # 반띵된 리스트 (여기다 껴넣기)  
+a = (nums[cut : ]) # 나눈 값 부터 슬라이싱 
+b = (nums[ :cut ]) # 반띵된 리스트 (여기다 껴넣기)  
+
+# n 빼먹어서 머리터짐 
+
+# 답
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        answer = []      
+        
+        for i in range(len(nums) // 2) :   
+            answer.append(nums[i])    
+            answer.append(nums[i+n])
+
+        return answer
+      
+      
+      
