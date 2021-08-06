0c711e33bd18c5e97f0a6c42b91d7ba94cb6e9aa
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-08-07 00:13:57 +0900

    Create 210807-9498.py

---
+++ 210807-9498.py
@@ -0,0 +1,14 @@
+score = int(input())
+
+# 검색참고해서 풀이
+
+if score >= 90 :      # 범위 지정 주의 
+    print('A')
+elif score >= 80 :
+    print('B')
+elif score >= 70:
+    print('C')
+elif score >= 60:
+    print('D')
+else : 
+    print('F')
