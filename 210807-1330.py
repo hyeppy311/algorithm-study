d0d18f2b088e8c0d8802c0fa46a6c861d30bec7b
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-08-07 00:08:55 +0900

    Create 210807-1330.py
    백준 if문

---
+++ 210807-1330.py
@@ -0,0 +1,12 @@
+a,b = map(int, input().split())
+
+def compare(A,B):
+    
+    if A > B :
+        print('>')
+    if A < B :
+        print('<')
+    if A == B :
+        print('==')
+
+compare(a,b)
