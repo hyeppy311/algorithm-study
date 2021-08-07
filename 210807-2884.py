c06d9bd2526ebf2cfcd0b275e647917a01711910
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-08-07 16:14:26 +0900

    if문 알람시계

--- 210807-2884.py
+++ 210807-2884.py
@@ -1,1 +1,12 @@
+h,m = map(int, input().split())
 
+a = m-45  # 분으로 판단하기 위해 변수 지정 
+
+if a < 0 :         # 분이 음수일경우   
+  if h == 0 :      # 00:00 ~ 00:45
+    print(23, m+15)  
+  else : 
+    print(h-1 , m + 15)
+    
+elif a >= 0 :  # 분이 양수일경우 
+ print(h, a)   # 시, 분 그대로 출력 
