5e7791b2219c22483af362db9f29ac3a6e663799
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-08-11 02:22:44 +0900

    617. Merge Two Binary Trees

---
+++ leetcode/210815-617.py
@@ -0,0 +1,15 @@
+# 문제
+
+# 이진트리 root1, root2 입력받는다.
+# root1와 root2 합치는데 겹치는 부분은 sum, 겹치지않으면 그대로 출력 
+
+# root는 리스트형식으로 입력됨 
+
+root1 =[]
+root2 =[]
+out = []
+
+for i in root1 :
+  for z in root2 :
+    out = i+z 
+    return out
