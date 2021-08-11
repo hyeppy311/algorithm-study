27775fc817dddf69bf6334c3eaf2a1efb49fe33a
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-08-12 00:10:27 +0900

    for문 X보다 작은 수

--- 백준/210815-10871.py
+++ 백준/210815-10871.py
@@ -1,9 +1,15 @@
 # 내가 푼 답
 
 n,x = map(int,input().split())
-a = list(range(1,1+n))
+a = list(range(1,1+n))   # n값을 받아와서 리스트를 만드는것임.. 
+
 
 for i in a : 
   if i < x :
     print(i, end = ' ')
     
+
+# 문제에서 요구하는 입력값
+10 5
+1 10 4 9 2 3 8 5 7 6 -> 이거 넣어줄 리스트 만들어야함..
+-> 이것으로 수정 a = list(map(int, input(),split())
