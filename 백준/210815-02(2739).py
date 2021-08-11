05acc1b91bcb3bf8301eaa5e8b75e599f564e264
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-08-12 00:24:52 +0900

    for문 구구단

--- 백준/210815-02(2739).py
+++ 백준/210815-02(2739).py
@@ -7,7 +7,7 @@ for i in num:
   print(n*i) # 이부분에서 포맷팅을 하면 될 것 같았는데.. 
 
   
-# 포맷팅 찾다가 발견한 코드 
+# 포맷팅 찾다가 발견한 코드.. 
 
 n = int(input())
 
@@ -15,17 +15,3 @@ for y in range(1, 10):
   print(f'{n} * {y} = {n*y}')
   
 
-
-# range 출력확인  
-for y in range(1, 10):
-  print(y)
->>>
-1
-2
-3
-4
-5
-6
-7
-8
-9
