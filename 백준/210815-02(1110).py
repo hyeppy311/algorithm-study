30bd7dc0210ec33425c4856ebe03348b9d46db27
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-08-16 00:50:52 +0900

    while문 더하기사이클

--- 백준/210815-02(1110).py
+++ 백준/210815-02(1110).py
@@ -1,1 +1,23 @@
+n = list(map(int,input()))
 
+a = n[0]
+b = n[1]
+
+cycle = 0
+
+
+while True: 
+  if a+b < 10 : 
+    c = list(format(a+b, '02'))
+    c_sum = c[0] + c[1]
+  else : 
+    a+b
+
+  sums = int(a+b)
+  sums1 = sums[0] + sums[1]
+  
+  cycle = cycle + 1 
+
+  if (n == sums1):
+    break
+    print(cycle)
