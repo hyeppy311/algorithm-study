f66a944caa12ab2608a46bb76cde2d8d8175be41
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-08-18 01:32:48 +0900

    while문 더하기사이클

--- 백준/210815-02(1110).py
+++ 백준/210815-02(1110).py
@@ -1,23 +1,20 @@
-n = list(map(int,input()))
+n = input()        ## 입,출력은 str 타입임
 
-a = n[0]
-b = n[1]
+if len(n) == 1 :   ## 입력값이 한 자릿수 일 경우
+  n = '0'+ n       ## 앞에 0을 붙여줌
+
+a = int(n[0])     ## string indexing. 
+b = int(n[1])
 
 cycle = 0
 
-
 while True: 
-  if a+b < 10 : 
-    c = list(format(a+b, '02'))
-    c_sum = c[0] + c[1]
-  else : 
-    a+b
-
-  sums = int(a+b)
-  sums1 = sums[0] + sums[1]
   
-  cycle = cycle + 1 
+  sums = (a+b)%10   ## 나머지 -> 일의 자리 수
+  a,b = b,sums      ## 다시 a에 b(일의자리수), b에 sums(합한값) 할당 
+                    
+  cycle = cycle + 1   ## 14,15라인 계속 돌고
 
-  if (n == sums1):
-    break
-    print(cycle)
+  if (n == 10*a+b):   ## a를 10의자리 수로 만들어서 b를 더해주고 처음 입력값이랑 같으면
+    print(cycle)      ## 출력하고 
+    break             ## 멈춤
