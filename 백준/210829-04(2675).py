e009a6cf70b7cfda36b89bfe3bb10a61cb540b71
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-08-26 01:20:23 +0900

    문자열 문자열반복

--- 백준/210829-04(2675).py
+++ 백준/210829-04(2675).py
@@ -1,1 +1,9 @@
+n = int(input())
 
+for _ in range(n) : 
+  a,b = input().split()
+  a = int(a)
+
+  for i in b : 
+    print(i*a, end = '')   ## 가로쓰기
+  print()                  ## Enter같은 역할을 함, 한 줄 띄우기 
