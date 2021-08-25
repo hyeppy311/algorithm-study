108cc1e7d50a3de2f692c480f1f5d0bf3f1e4d04
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-08-26 00:28:44 +0900

    문자열 알파벳찾기

---
+++ 백준/210829.py
@@ -0,0 +1,16 @@
+import string 
+
+n = list(input())
+
+letter = list(string.ascii_lowercase)
+
+a = [-1]*26
+
+for idx,i in enumerate(letter):
+  try:
+    a[idx] = n.index(i)
+  except:
+    pass
+
+for i in a:
+  print(i, end=' ')
