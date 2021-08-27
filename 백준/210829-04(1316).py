649ea621aefe99bd116cfc8ca8c728ecb9517dfd
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-08-28 02:29:26 +0900

    문자열 그룹 단어 체커

---
+++ 백준/210829-04(1316).py
@@ -0,0 +1,24 @@
+n = int(input())
+
+answer = 0
+
+for _ in range(n) :
+  
+  letter = input()
+  temp = str()
+  st = set()
+
+  for i in letter :
+    if i not in st :
+      st = st | {i}
+      temp = i 
+    elif i == temp :
+      pass
+    else : 
+      st = False 
+      break
+  if st :
+    answer += 1
+
+print(answer)
+
