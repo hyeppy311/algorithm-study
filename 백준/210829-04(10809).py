5e8c193aa016a2251a7c63e3a5ef822ebd29a960
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-08-29 17:18:59 +0900

    문자열 알파벳찾기

--- 백준/210829-04(10809).py
+++ 백준/210829-04(10809).py
@@ -1,18 +1,21 @@
-import string 
+## 문제) 입력값의 알파벳이 등장하는 인덱스 표시 
+
+
+import string   
 
 n = list(input())
 
-letter = list(string.ascii_lowercase)
+letter = list(string.ascii_lowercase)     ## a~z까지 리스트에 담기. 
 
-a = [-1]*26
+a = [-1]*26                               ## -1로된 리스트 만들기
 
-for idx,i in enumerate(letter):
+for idx,i in enumerate(letter):          ## enumerate : 들어온 인자의 인덱스와 원소에 차례대로 접근
   try:
-    a[idx] = n.index(i)
+    a[idx] = n.index(i)                 ## 리스트 a에 입력단어 인덱스값 할당
   except:
     pass
 
-for i in a:
+for i in a:                             ## 리스트 a 원소출력
   print(i, end=' ')
   
   
