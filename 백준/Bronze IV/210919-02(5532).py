b6813e047f9196a24005cc13c2ca69f52968cdf9
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-09-18 23:59:52 +0900

    방학숙제

--- 백준/Bronze IV/210919-02(5532).py
+++ 백준/Bronze IV/210919-02(5532).py
@@ -1,1 +1,31 @@
 
+l,a,b,c,d = [int(input()) for _ in range(5)]
+
+
+if a//c > b//d :
+  if a%c == 0 :
+    print(l-a//c)
+  else :
+    print(l-(a//c +1))
+
+elif a//c == b//d :
+  if a%c == 0 and b%d == 0 :
+    print(l-a//c)
+  else :
+    print(l-(a//c +1))
+
+else :
+  if b%d == 0 :
+    print(l-b//d)
+  else :
+    print(l- (b//d +1))
+    
+    
+ ## 짧은코드
+
+import math
+
+l,a,b,c,d = [int(input()) for _ in range(5)]
+
+print(l-max(math.ceil(a/c),math.ceil(b/d))) ## ceil : 나머지 올림
+
