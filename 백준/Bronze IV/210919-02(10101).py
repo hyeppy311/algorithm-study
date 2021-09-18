13ee89e0f47fa4838250eb465708b56f6543c0c9
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-09-19 00:24:59 +0900

    삼각형 외우기

--- 백준/Bronze IV/210919-02(10101).py
+++ 백준/Bronze IV/210919-02(10101).py
@@ -1,1 +1,20 @@
 
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a == 60 and b == 60 and c == 60 :
+  print('Equilateral')
+
+elif a+b+c == 180 and (a==b or b==c or a==c) :
+  print('Isosceles')
+
+elif a+b+c ==180 and (a!=b or b!=c or a!=c) :
+  print('Scalene')
+
+elif a+b+c != 180 :
+  print('Error')
+
+
+  
+  
