64bc61e554c225606e463a6793f76ae215b0cec7
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-09-19 00:18:37 +0900

    주사위 세개

--- 백준/Bronze IV/210919-02(2480).py
+++ 백준/Bronze IV/210919-02(2480).py
@@ -1,1 +1,16 @@
 
+a,b,c = map(int, input().split())
+
+if a == b == c :
+  print(a*1000+10000)
+elif a == b :
+  print(a*100+1000)
+elif a == c :
+  print(a*100+1000)
+elif b == c :
+  print(b*100+1000)
+else : 
+  print(max(a,b,c)*100)
+  
+  
+  
