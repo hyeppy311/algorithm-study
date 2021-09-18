f85cad3a134ab11a0ea1020d1b513e33c6be642f
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-09-19 00:26:06 +0900

    시험 점수

--- 백준/Bronze IV/210919-02(5596).py
+++ 백준/Bronze IV/210919-02(5596).py
@@ -1,1 +1,14 @@
 
+minkuk = list(map(int, input().split()))
+manse = list(map(int, input().split()))
+
+s = sum(minkuk)
+t = sum(manse)
+
+if s == t :
+  print(s)
+else : 
+  print(max(s,t))
+  
+  
+  
