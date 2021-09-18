b0a924910156c18eb6af4a6b76add319e9b7867e
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-09-19 00:23:35 +0900

    인공지능 시계

--- 백준/Bronze IV/210919-02(2530).py
+++ 백준/Bronze IV/210919-02(2530).py
@@ -1,1 +1,15 @@
 
+h, m, s = map(int,input().split())
+seconds = int(input())
+
+hour = h*3600
+minutes = m*60
+sec = s
+
+
+print(((hour+minutes+sec+seconds)//3600)%24,(hour+minutes+sec+seconds)%3600//60, 
+(hour+minutes+sec+seconds)%3600%60%60)
+
+
+
+## 변수명 짧게쓸순없을까..? 
