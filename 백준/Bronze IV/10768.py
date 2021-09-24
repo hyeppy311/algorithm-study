898b0fe803cad1fb6b8724263934f809a890eb32
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-09-24 17:21:46 +0900

    Create 10768.py

---
+++ 백준/Bronze IV/10768.py
@@ -0,0 +1,32 @@
+
+
+## 내가 쓴 코드 
+
+month = int(input())
+day = int(input())
+
+if month == 1 and 1 <= day <= 31:
+  print("Before")
+
+elif month == 2 and 1 <= day <= 17 :
+  print('Before')
+  
+elif month == 2 and day == 18 :
+  print("Special")
+
+elif month == 2 and day > 18 :
+  print("After")
+
+elif month > 2 and 1<= day <= 31: 
+  print("After")
+  
+ 
+
+## 짧은 코드
+
+if month == 2 and day == 18 :
+  print('Special')
+elif month < 2 or (month == 2 and day <= 17) :    ## 1월은 무조건 Before가 되므로 day조건을 걸어줄 필요가 없음
+  print("Before")
+else :
+  print('After')
