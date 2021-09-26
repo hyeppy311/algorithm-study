817defb350ddde09a7c389e794bc32a4f7217fb1
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-09-26 23:29:43 +0900

    과목선택

--- 백준/Bronze IV/11948.py
+++ 백준/Bronze IV/11948.py
@@ -1,3 +1,5 @@
+## 정렬후 맨 앞의 값(최소) 제거
+
 a,b,c,d,e,f = [int(input()) for _ in range(6)]
 
 sub_1 = [a,b,c,d]
@@ -8,3 +10,16 @@ sub_1.pop(0)
 sub_2.pop(0)
 
 print(sum(sub_1)+sum(sub_2))
+
+
+
+## 다 더한 후 최소값 제거
+
+a,b,c,d,e,f = [int(input()) for _ in range(6)]
+
+sub_1 = [a,b,c,d]
+sub_2 = [e,f]
+
+print(sum(sub_1)+sum(sub_2)-min(sub_1)-min(sub_2))
+
+
