d5125e25e7a6c7b636b5e0436adab79cc46ec777
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-10-08 22:02:49 +0900

    문자열 분석

---
+++ 백준/Bronze II/211010-02(10820).py
@@ -0,0 +1,30 @@
+## 코드 검색 
+
+while True:
+    try:
+        lower_case, upper_case, number, blank = 0, 0, 0, 0
+ 
+        for i in input():
+            if i.islower():
+                lower_case += 1
+            elif i.isupper():
+                upper_case += 1
+            elif i.isdigit():
+                number += 1
+            else:
+                blank += 1
+        print(lower_case, upper_case, number, blank)
+ 
+    except EOFError:      ## 입력이 끝날 때까지 값을 받고 끝나면 break
+        break
+      
+      
+## 입력값의 갯수가 지정되있지 않기 때문에 while문 으로 입력 받기.
+## 소문자, 대문자, 숫자, 공백 변수지정 
+
+## Boolean type
+## islower(): 영어소문자
+## isupper(): 영어대문자
+## isdigit(): 숫자 
+      
+## EOFError : End Of File 파일의 끝. 
