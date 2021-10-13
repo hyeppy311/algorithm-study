347b9783f6ed49020194ae6da37533e250fad3a6
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-10-13 21:55:51 +0900

    Create 15873.py

---
+++ 백준/Bronze IV/15873.py
@@ -0,0 +1,20 @@
+## 공백없는 A+B
+
+## 1
+number = list(input())
+
+
+if len(number) == 2 :
+  print(int(number[0])+int(number[1]))
+
+elif len(number) == 3 :
+  print(int(number[0]+number[1]) + int(number[2]))
+  
+## 예제만 보고 3자리 수 까지만 구함 -> 틀! 
+
+
+
+
+## 2 (3자리 수 이상 되는 것은 어떻게 해야될까,...) 
+
+
