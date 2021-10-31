480daa42dfc9dda28158ad00f76d112ebd3fc08d
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-10-31 22:35:33 +0900

    TNG

---
+++ 백준/Bronze III/5063.py
@@ -0,0 +1,16 @@
+
+
+n = int(input())
+
+for _ in range(n) :
+  r,e,c = map(int,input().split())
+
+  if r < e-c :
+    print("advertise")
+  elif r == e-c :
+    print("does not matter")
+  else :
+    print("do not advertise")
+    
+    
+    
