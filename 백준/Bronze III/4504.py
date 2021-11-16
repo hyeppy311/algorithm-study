a2d01126ad1bf1662033ac656095857d274489c1
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-11-16 21:19:56 +0900

    배수찾기

---
+++ 백준/Bronze III/4504.py
@@ -0,0 +1,24 @@
+## 시도한 코드
+
+n = int(input())
+
+while True :
+  num = int(input())
+
+  if num == 0 :
+    break 
+    
+  if num % n == 0 :
+    print(f'{num} is a multiple of 3.')   ## 3아니고...n으로 받아야지.. 
+
+  else :
+    print(f'{num} is NOT a multiple of 3.')
+
+
+ ## 그리고 11~15라인 8라인 if문에 indent 맞추기 
+
+
+ 
+  
+  
+  
