44a597c32dee80243adbebc1a65205111a7a8ecb
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-12-01 21:35:28 +0900

    숫자의 합

---
+++ 백준/Bronze II/11720.py
@@ -0,0 +1,22 @@
+
+
+n = int(input())
+nums = input()
+st = list(nums)
+
+ls = []
+
+for i in nums :
+  ls.append(int(i))
+print(sum(ls))
+
+
+------------------------------
+
+# 전에 풀었던 코드 
+
+n = int(input())
+number = list(map(int, input()))
+
+print(sum(number))
+
