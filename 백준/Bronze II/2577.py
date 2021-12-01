904c97c6514352c6bc68f1316ad5ab75650cc380
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-12-01 20:41:19 +0900

    Update 2577.py

--- 백준/Bronze II/2577.py
+++ 백준/Bronze II/2577.py
@@ -48,6 +48,33 @@ seven ,
 eight , 
 nine , sep='\n')
 
-## 처음제출에서는 srt.count()ㄹ
-ㅡ
-ㅡㄹ
+## 처음제출에서는 srt.count()를 썼음 
+
+## 짧은 코드 검색 
+
+answer = 1
+for _ in range(3):
+  answer *= int(input())   # 입력받으면서 곱해주기 
+lst = [0] * 10             # 0으로 된 리스트 만들어주기 
+for i in str(answer):      # 입력받은 숫자를 문자로 변환 for문으로 돌리고
+  lst[int(i)] += 1         # 0부터 개수 출력이므로 인덱스에 맞게 숫자 카운트  
+for i in lst:
+  print(i)                 # 개수 넣어준 리스트 출력
+  
+----------------------------------------------------------------------
+# 이것은 내가 푼것같은뎁..? 
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+m = str(a*b*c)
+
+for i in range(10) :
+    print(m.count(str(i)))
+    
+
+
+  
+
+
