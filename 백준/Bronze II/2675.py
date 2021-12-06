2f7b190a818b6711bd02c1ac91b714b2af0a24b0
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-12-06 20:38:29 +0900

    문자열 반복

---
+++ 백준/Bronze II/2675.py
@@ -0,0 +1,26 @@
+
+## 시도한 코드 
+
+
+n = int(input())
+
+for _ in range(n) :
+  times, letter = input().split()
+
+  for i in letter :
+    print(i * int(times), end = '')
+   
+  ## print() 줄바꿈 추가 해줘야함...
+    
+    
+ ## 이전에 제출한 코드 
+
+n = int(input())
+
+for _ in range(n) : 
+  a,b = input().split()
+  a = int(a)
+
+  for i in b : 
+    print(i*a, end = '')
+  print() 
