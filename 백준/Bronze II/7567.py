7888824822a31370bb0889099204d56296480236
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2021-12-15 21:20:21 +0900

    그릇

--- 백준/Bronze II/7567.py
+++ 백준/Bronze II/7567.py
@@ -12,3 +12,41 @@ for i in n :
     height += 10 
   
 print(height+5)
+
+
+## 처음 오는 그릇의 모양과 그 다음의 모양을 판단 하는 코드를 짜야함 
+# 1. (( 일 경우
+# 2. () 일 경우
+# 3. )) 일 경우
+# 4. )( 일 경우
+
+for i in range(len(n)) :
+  if n[i] == n[i+1] :           ## 인덱스 범위 넘어가버림
+    height += 5
+  else : 
+    height += 10    
+
+
+# range 범위 줄이기
+
+for i in range(0,len(n)-1) :
+  if n[i] == n[i+1] :
+    height += 5
+  else : 
+    height += 10
+
+print(height)
+
+
+## 정답!! 
+
+n = list(input())
+height = 10                   ## 처음 그릇의 높이 10에서
+  
+for i in range(0,len(n)-1) :
+  if n[i] == n[i+1] :         ## 그릇 모양이 같으면 (( +5
+    height += 5
+  else :                      ## 다르면 () +10
+    height += 10
+
+print(height)
