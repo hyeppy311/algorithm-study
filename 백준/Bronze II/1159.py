ea33610fa2059bb5658d993a60ad7edf3fb4e20f
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2022-01-22 16:58:42 +0900

    농구 경기

---
+++ 백준/Bronze II/1159.py
@@ -0,0 +1,15 @@
+
+
+n = int(input())
+
+tmp = dict()
+for _ in range(n) :
+  name = input()[0]         # 첫 글자만 받기
+  if tmp.get(name):         # key가 딕셔너리에 있는경우 대응하는 value값을 돌려줌
+    tmp[name] += 1          # key에 대응하는 value값을 하나씩 추가
+  else: tmp[name] = 1       # value값을 1로 만들어 
+
+
+lst = sorted([k for k,v in tmp.items() if v >= 5])  # k는 key, v는 value로 설정. v의 값이 5이상이 되는 k값을 리스트에 저장
+
+print("".join(lst))
