1bb2b3417c52e6d9d280178eba68399875442bfb
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2022-03-03 23:05:15 +0900

    대표값

--- 백준/Bronze II/2592.py
+++ 백준/Bronze II/2592.py
@@ -26,5 +26,7 @@ print(cnt)
 Counter({30: 3, 40: 2, 60: 2, 10: 1, 20: 1, 50: 1})
 
 
-
+numbers = [int(input()) for i in range(10)]
+print(sum(numbers)//10)
+print(max(numbers, key = numbers.count))
 
