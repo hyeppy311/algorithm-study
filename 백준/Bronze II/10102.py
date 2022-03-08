7b5fd24db990f2ee7fc22a70e642690fec07e23d
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2022-03-09 04:17:53 +0900

    개표

---
+++ 백준/Bronze II/10102.py
@@ -0,0 +1,13 @@
+
+
+v = int(input())
+votes = input()
+
+if votes.count("A") > votes.count("B") :
+  print("A")
+elif votes.count("A") < votes.count("B") :
+  print("B")
+else :
+  print("Tie")
+  
+  
