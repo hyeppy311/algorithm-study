71d67721b9932201a79a7141260d2ffa8e0f7088
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2022-04-05 21:43:51 +0900

    직사각형 (2차원 배열)

--- 백준/Bronze I/2669.py
+++ 백준/Bronze I/2669.py
@@ -5,4 +5,38 @@
 # 사각형의 내부를 1로 쪼개서 2차원 리스트에 저장 
 
 
+# 시도 ==> 답은 나왔는데 틀렸음 
+
+for _ in range(4) :
+  L_x,L_y,R_x,R_y = map(int, input().split())
+  
+  tmp = [[0] * 100 for _ in range(100)]  # range 범위 잘못됨 
+
+  for i in range(R_x-L_x-1, R_x+1):    # 오른쪽 위 꼭지점으로 사각형을 카운트했는데 굳이 이렇게 할 필요 없었음  
+    for j in range(R_y-L_y, R_y+1) :
+      tmp[j][i] = 1
+
+cnt = 0
+for i in tmp :
+  cnt += sum(i)
+print(cnt)
+
+
+# 검색
+
+paper = [[0 for _ in range(101)] for _ in range(101)]    
+
+for _ in range(4):
+    x1, y1, x2, y2 = map(int, input().split())
+    
+    #사각형 부분만 1로 바꾸어줌
+    for i in range(x1, x2):          # 입력받은 좌표를 그냥 넣어주면 됨 
+        for j in range(y1, y2):
+            paper[i][j] = 1
+
+answer = 0
+for row in paper:
+    answer += sum(row)
+print(answer)
+
 
