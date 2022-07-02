9c0a56c110da2548dcd03b4dccbf9bed4ec3927b
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2022-07-03 01:24:15 +0900

    초심자의 회문검사

--- swexpertacademy/D2-1989.py
+++ swexpertacademy/D2-1989.py
@@ -10,3 +10,14 @@ for t in range(1,T+1) :
   else : 
     print(f'#{t} {0}')
           
+
+      
+T = int(input())
+
+for t in range(1,T+1) :
+  word = input().strip()
+
+  if word == word[::-1].strip() :
+    print(1)
+  else: 
+    print(0)
